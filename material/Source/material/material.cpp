324114d2072beadc80b0bb0c2d180a36f6e4db09
Theredhawksings <myeoseul@gmail.com>
2025-12-22 00:03:58 +0900

    16주차 작업일지

--- material/Source/material/material.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "material.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, material, "material" );
-
-DEFINE_LOG_CATEGORY(Logmaterial)
--- material/Source/material/materialGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "materialGameMode.h"
-
-AmaterialGameMode::AmaterialGameMode()
-{
-	// stub
-}
